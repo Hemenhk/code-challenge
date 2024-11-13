0b3cc17855321c4e315ace02da1ca385f81566bf
Hemenhk <hemen.hiwakamal@hotmail.com>
2024-11-13 15:57:55 +0100

    first commit

---
+++ src/providers/tanstack-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ReactNode } from "react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { getQueryClient } from "@/utils/get-query-client";
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
